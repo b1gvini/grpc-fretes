30c6d61caf213fa848c1d1c92f96979de353a6e8
Vinicius de Franca <vgf.vinicius@gmail.com>
2021-07-07 10:00:00 -0300

    adicionado tratamento de erros e validacao

--- src/main/kotlin/br/com/zup/FretesGrpcServer.kt
+++ src/main/kotlin/br/com/zup/FretesGrpcServer.kt
@@ -1,7 +1,14 @@
 package br.com.zup
 
+import com.google.protobuf.Any
+import com.google.rpc.Code
+import com.google.rpc.StatusProto
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
+import java.lang.Exception
+import java.lang.IllegalStateException
 import javax.inject.Singleton
 import kotlin.random.Random
 
@@ -14,9 +21,57 @@ class FretesGrpcServer: FretesServiceGrpc.FretesServiceImplBase() {
 
         logger.info("Calculando frete para o request: ${request}")
 
+        val cep = request?.cep
+        if(cep == null || cep.isBlank()){
+            val error =  Status.INVALID_ARGUMENT
+                            .withDescription("Cpf deve ser informado")
+                            .asRuntimeException()
+            responseObserver?.onError(error)
+        }
+
+        if(!cep!!.matches("[0-9]{5}-[0-9]{3}".toRegex())){
+            val error = Status.INVALID_ARGUMENT
+                .withDescription("Cpf no formato Invalido")
+                .augmentDescription("O formato esperado deve ser 99999-999")
+                .asRuntimeException()
+            responseObserver?.onError(error)
+        }
+
+        //SIMULAR ERRO DE SEGURANCA
+
+        if(cep.endsWith("333")){
+
+            val statusProto = com.google.rpc.Status.newBuilder()
+                                .setCode(Code.PERMISSION_DENIED.number)
+                                .setMessage("Usuario nao pode acessar este recurso")
+                                .addDetails(Any.pack(ErrorDetails.newBuilder()
+                                    .setCode(401)
+                                    .setMessage("Token Invalido")
+                                    .build()))
+                                .build()
+            // DETALHES DO ADDDETAILS VAI NOS METADADOS
+            // ( NO BLOOM RPC AINDA N E POSSIVEL VISUALIZAR OS METADADOS RECEBIDOS)
+            val error = io.grpc.protobuf.StatusProto.toStatusRuntimeException(statusProto)
+            responseObserver?.onError(error)
+        }
+
+        var valor = 0.0
+        try {
+            valor = Random.nextDouble(from = 15.00, until = 200.00)
+            if(valor > 100){
+                throw IllegalStateException("Erro inesperado no sistema")
+            }
+        }catch (e: Exception){
+            responseObserver?.onError(Status.INTERNAL
+                .withDescription(e.message)
+                .withCause(e)
+                .asRuntimeException()
+            )
+        }
+
         val response = CalculaFreteResponse.newBuilder()
             .setCep(request!!.cep)
-            .setFrete(Random.nextDouble(from = 15.00, until = 200.00))
+            .setFrete(valor)
             .build()
         logger.info("Frete calculado: ${response}")
         responseObserver!!.onNext(response)
